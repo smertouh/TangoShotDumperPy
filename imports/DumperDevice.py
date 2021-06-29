19e9453aa6185370fe57919117f295194c1f1a95
salinsalins <sanin_andrey@mail.ru>
2021-06-29 14:41:05 +0700

    commit

--- imports/DumperDevice.py
+++ imports/DumperDevice.py
@@ -29,17 +29,18 @@ class DumperDevice:
                 length = len(self.y)
             x_name = self.name.replace('y', 'x')
             if x_name == self.name:
-                self.x = numpy.linspace(0, length, length, dtype=numpy.float32)
+                self.x = None
                 return self.x
             try:
                 self.x_attr = self.device.read_attribute(x_name)
                 self.x = self.x_attr.value
                 return self.x
             except:
-                self.x = numpy.linspace(0, length, length, dtype=numpy.float32)
+                self.x = None
                 return self.x
 
         def properties(self):
+            # returns dictionary with attribute properties for channel attribute
             try:
                 db = self.device.get_device_db()
                 return db.get_device_attribute_property(self.device.name(), self.name)[self.name]
@@ -65,9 +66,114 @@ class DumperDevice:
                         pass
             return marks
 
-    def __init__(self, tango_device_name: str, folder=''):
+        def save_log(self, log_file):
+            properties = self.properties()
+            # Signal label = default mark name
+            label = properties.get('label', [''])[0]
+            if '' == label:
+                label = properties.get('name', [''])[0]
+            if '' == label:
+                label = self.name
+            # Units
+            unit = properties.get('unit', [''])[0]
+            # coefficient for conversion to units
+            coeff = float(properties.get('display_unit', ['1.0'])[0])
+            # process marks
+            marks = self.get_marks()
+            # Find zero value
+            zero = marks.get('zero', 0.0)
+            # Convert all marks to mark_value = (mark - zero)*coeff
+            for mark in marks:
+                first_line = True
+                # If it is not zero mark
+                if not "zero" == mark:
+                    mark_value = (marks[mark] - zero) * coeff
+                    mark_name = mark
+                    # Default mark renamed to label
+                    if mark_name == "mark":
+                        mark_name = label
+                    # Print mark name = value
+                    if first_line:
+                        print("%10s " % self.name, end='')
+                        first_line = False
+                    else:
+                        print("%10s " % "  ", end='')
+                    # printed mark name
+                    pmn = mark_name
+                    if len(mark_name) > 14:
+                        pmn = mark_name[:5] + '...' + mark_name[-6:]
+                    # print mark vavue
+                    if abs(mark_value) >= 1000.0:
+                        print("%14s = %7.0f %s\r\n" % (pmn, mark_value, unit), end='')
+                    elif abs(mark_value) >= 100.0:
+                        print("%14s = %7.1f %s\r\n" % (pmn, mark_value, unit), end='')
+                    elif abs(mark_value) >= 10.0:
+                        print("%14s = %7.2f %s\r\n" % (pmn, mark_value, unit), end='')
+                    else:
+                        print("%14s = %7.3f %s\r\n" % (pmn, mark_value, unit), end='')
+                    # output data format
+                    format = properties.get('format', ['%6.2f'])[0]
+                    out_str = "; %s = " % mark_name + format % mark_value + " %s" % unit
+                    log_file.write(out_str)
+
+        def save_properties(self, zip_file, folder, device_name):
+            zip_entry = folder + "properties_" + self.name + ".txt"
+            buf = "Signal_Name=%s/%s\r\n" % (device_name, self.name)
+            properties = self.properties()
+            for prop in properties:
+                buf += '%s=%s\r\n' % (prop, properties[prop][0])
+            zip_file.writestr(zip_entry, buf)
+
+        def save_data(self, zip_file, folder):
+            zip_entry = folder +  self.name + ".txt"
+            avg = int(self.properties().get("save_avg", ['1'])[0])
+            outbuf = ''
+            if self.x is None:
+                # save only y values
+                fmt = '%f'
+                fmtcrlf = fmt + '\r\n'
+                n = len(self.y)
+                ys = 0.0
+                ns = 0.0
+                for k in range(n):
+                    ys += self.y[k]
+                    ns += 1.0
+                    if ns >= avg:
+                        s = fmtcrlf % (ys / ns)
+                        outbuf += s.replace(",", ".")
+                        ys = 0.0
+                        ns = 0.0
+                if ns > 0:
+                    s = fmt % (ys / ns)
+                    outbuf += s.replace(",", ".")
+            else:
+                # save "x; y" pairs
+                fmt = '%f; %f'
+                fmtcrlf = fmt + '\r\n'
+                n = min(len(self.x), len(self.x))
+                xs = 0.0
+                ys = 0.0
+                ns = 0.0
+                for k in range(n):
+                    xs += self.x[k]
+                    ys += self.y[k]
+                    ns += 1.0
+                    if ns >= avg:
+                        s = fmtcrlf % (xs / ns, ys / ns)
+                        outbuf += s.replace(",", ".")
+                        xs = 0.0
+                        ys = 0.0
+                        ns = 0.0
+                if ns > 0:
+                    s = fmt % (xs / ns, ys / ns)
+                    outbuf += s.replace(",", ".")
+            zip_file.writestr(zip_entry, outbuf)
+
+    def __init__(self, tango_device_name: str, folder='./'):
         self.logger = self.config_logger(name=__qualname__, level=logging.DEBUG)
         self.name = tango_device_name
+        if not folder.endswith('/'):
+            folder += '/'
         self.folder = folder
         self.active = False
         self.tango_device = None
--- imports/PicoLog1000.py
+++ imports/PicoLog1000.py
@@ -1,31 +1,16 @@
-import time
-import logging
-
-import numpy
-import tango
-
 from imports.DumperDevice import *
 
 
 class PicoLog1000(DumperDevice):
-    class Channel(DumperDevice.Channel):
-        pass
-
     def __init__(self, tango_device_name: str, folder='PicoLog'):
         super().__init__(tango_device_name, folder)
 
     def save(self, log_file, zip_file):
-        # read y ready
+        # read data ready
         data_ready = self.tango_device.read_attribute('data_ready').value
         if not data_ready:
             self.logger.warning("%s y is not ready" % self.name)
             return
-        # read raw y
-        raw_data = self.tango_device.read_attribute('raw_data').value
-        # read other attributes
-        trigger = self.tango_device.read_attribute('trigger').value
-        sampling = self.tango_device.read_attribute('sampling').value
-        points = self.tango_device.read_attribute('points_per_channel').value
         # read channels list
         channels = self.tango_device.read_attribute('channels').value
         channels_list = []
@@ -33,104 +18,36 @@ class PicoLog1000(DumperDevice):
             channels_list = eval(channels)
         except:
             pass
-
+        if len(channels_list) <= 0:
+            self.logger.warning("%s empty channels list" % self.name)
+            return
+        # read other attributes
+        trigger = self.tango_device.read_attribute('trigger').value
+        sampling = self.tango_device.read_attribute('sampling').value
+        points = self.tango_device.read_attribute('points_per_channel').value
         # generate times array
-        t = numpy.linspace(0, (points - 1) * sampling, points, dtype=numpy.float32)
-        times = numpy.zeros(raw_data.shape, dtype=numpy.float32)
-        for i, chan in enumerate(channels_list):
-            times[i, :] = t + (i * sampling / len(channels_list))
+        times = numpy.linspace(0, (points - 1) * sampling, points, dtype=numpy.float32)
         if trigger < points:
-            trigger_offset = times[0, trigger]
+            trigger_offset = times[trigger]
             times -= trigger_offset
-        #attr_list = self.tango_device.get_attribute_list()
-        #prop_list = self.property_list()
-        for i, chan in enumerate(channels_list):
+        # save channels data and properties
+        for i, number in enumerate(channels_list):
             try:
-                name = "channel_%03i" % chan
-                prop = self.tango_device.get_property(name)[name]
-                sdf = "save_data" in prop
-                slf = "save_log" in prop
-                # save signal properties
+                chan = PicoLog1000.Channel(self.tango_device, number)
+                properties = chan.properties()
+                # read flags
+                sdf = self.as_boolean(properties.get("save_data", ['False'])[0])
+                slf = self.as_boolean(properties.get("save_log", ['False'])[0])
+                # save signal properties and data
                 if sdf or slf:
-                    self.save_prop(zip_file, chan)
-                    self.save_log(log_file, chan)
+                    chan.save_properties(zip_file, self.folder, self.name)
+                    chan.save_log(log_file)
                     if sdf:
-                        self.save_data(zip_file, chan)
+                        # read data
+                        chan.read_y()
+                        # generate times values
+                        chan.x = times + (i * sampling / len(channels_list))
+                        chan.save_data(zip_file, self.folder)
             except:
-                self.logger.warning("%s y save exception" % self.name)
+                self.logger.warning("%s save exception" % self.name)
                 self.logger.debug('', exc_info=True)
-
-    def save_data(self, zip_file, chan):
-        entry = chan.dev.folder + "/" + chan.name + ".txt"
-        avg = chan.get_prop_as_int("save_avg")
-        if avg < 1:
-            avg = 1
-        if chan.x is None or len(chan.x) != len(chan.attr.value):
-            chan.x = chan.read_x_data()
-        buf = convert_to_buf(chan.x, chan.attr.value, avg)
-        zip_file.writestr(entry, buf)
-
-    def save_prop(self, zip_file, chan):
-        entry = chan.dev.folder + "/" + "param" + chan.name + ".txt"
-        buf = "Signal_Name=%s/%s\r\n" % (chan.dev.get_name(), chan.name)
-        buf += "Shot=%d\r\n" % chan.dev.shot
-        prop_list = ['%s=%s' % (k, chan.prop[k][0]) for k in chan.prop]
-        for prop in prop_list:
-            buf += "%s\r\n" % prop
-        zip_file.writestr(entry, buf)
-
-    def save_log(self, log_file, chan):
-        # Signal label = default mark name
-        label = chan.get_prop('label')
-        if label is None or '' == label:
-            label = chan.get_prop('name')
-        if label is None or '' == label:
-            label = chan.name
-        # Units
-        unit = chan.get_prop('unit')
-        # Calibration coefficient for conversion to units
-        coeff = chan.property_as_boolean("display_unit")
-        if coeff is None or coeff == 0.0:
-            coeff = 1.0
-
-        marks = chan.get_marks()
-
-        # Find zero value
-        zero = 0.0
-        if "zero" in marks:
-            zero = marks["zero"]
-        # Convert all marks to mark_value = (mark - zero)*coeff
-        for mark in marks:
-            first_line = True
-            # If it is not zero mark
-            if not "zero" == mark:
-                mark_value = (marks[mark] - zero) * coeff
-                mark_name = mark
-                # Default mark renamed to label
-                if mark_name == "mark":
-                    mark_name = label
-                # Print mark name = value
-                if first_line:
-                    print("%10s " % chan.name, end='')
-                    first_line = False
-                else:
-                    print("%10s " % "  ", end='')
-                pmn = mark_name
-                if len(mark_name) > 14:
-                    pmn = mark_name[:5] + '...' + mark_name[-6:]
-                if abs(mark_value) >= 1000.0:
-                    print("%14s = %7.0f %s\r\n" % (pmn, mark_value, unit), end='')
-                elif abs(mark_value) >= 100.0:
-                    print("%14s = %7.1f %s\r\n" % (pmn, mark_value, unit), end='')
-                elif abs(mark_value) >= 10.0:
-                    print("%14s = %7.2f %s\r\n" % (pmn, mark_value, unit), end='')
-                else:
-                    print("%14s = %7.3f %s\r\n" % (pmn, mark_value, unit), end='')
-
-                format = chan.get_prop('format')
-                if format is None or '' == format:
-                    format = '%6.2f'
-                outstr = "; %s = " % mark_name + format % mark_value + " %s" % unit
-                log_file.write(outstr)
-        outstr = "; SHOT_TIME = %f" % self.read_shot_time()
-        log_file.write(outstr)
