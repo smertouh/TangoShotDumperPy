5a6cfd3923c75034c8df70c909557d1d27c8dc3e
salinsalins <sanin_andrey@mail.ru>
2021-07-01 13:51:16 +0700

    commit

--- PicoLog1000.py
+++ PicoLog1000.py
@@ -2,14 +2,15 @@ from DumperItem import *
 
 
 class PicoLog1000(DumperItem):
-    def __init__(self, tango_device_name: str, folder='PicoLog'):
-        super().__init__(tango_device_name, folder)
+    def __init__(self, tango_device_name: str, zip_folder='PicoLog'):
+        super().__init__(tango_device_name)
+        self.folder = zip_folder
 
     def save(self, log_file, zip_file, zip_folder=''):
         # read data ready
         data_ready = self.device.read_attribute('data_ready').value
         if not data_ready:
-            self.logger.warning("%s y is not ready" % self.name)
+            self.logger.warning("%s is not ready" % self.name)
             return
         # read channels list
         channels = self.device.read_attribute('channels').value
@@ -40,14 +41,15 @@ class PicoLog1000(DumperItem):
                 slf = self.as_boolean(properties.get("save_log", ['False'])[0])
                 # save signal properties and data
                 if sdf or slf:
-                    chan.save_properties(zip_file, self.zip_folder)
+                    # read data
+                    chan.read_y()
+                    # generate times values
+                    chan.x = times + (i * sampling / len(channels_list))
+                    chan.save_properties(zip_file, self.folder)
+                if slf:
                     chan.save_log(log_file)
-                    if sdf:
-                        # read data
-                        chan.read_y()
-                        # generate times values
-                        chan.x = times + (i * sampling / len(channels_list))
-                        chan.save_data(zip_file, self.zip_folder)
+                if sdf:
+                    chan.save_data(zip_file, self.folder)
             except:
                 self.logger.warning("%s save exception" % self.name)
                 self.logger.debug('', exc_info=True)
