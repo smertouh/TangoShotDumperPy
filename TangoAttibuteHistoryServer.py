1cdc5deba3a115ff0f97904414811f2a7d1f02b9
salinsalins <sanin_andrey@mail.ru>
2021-07-12 13:37:00 +0700

    commit

--- TangoAttibuteHistoryServer.py
+++ TangoAttibuteHistoryServer.py
@@ -21,7 +21,7 @@ from tango.server import Device, attribute, command, pipe, device_property
 from TangoServerPrototype import TangoServerPrototype, Configuration
 
 EMPTY_HISTORY = numpy.empty((0, 2))
-
+SERVER_CONFIG = ('log_level', 'config_file')
 
 class TangoAttributeHistoryServer(TangoServerPrototype):
     version = '0.0'
@@ -52,7 +52,7 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
             # read config from file and set config
             config_file = self.get_device_property('config_file', 'TangoAttributeHistoryServer.json')
             self.config = Configuration(config_file)
-            self.set_config()
+            self.set_server_config()
             # # read and set shot time
             # t = self.get_device_property('shot_time', 0.0)
             # self.write_shot_time(t)
@@ -60,7 +60,7 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
             self.attributes = {}
             properties = self.properties()
             for prop in properties:
-                if prop not in ('log_level', 'config_file'):
+                if prop not in SERVER_CONFIG:
                     try:
                         params = json.loads(properties[prop])
                         self.attributes[prop] = self.configure_attribute(prop, params)
@@ -81,7 +81,7 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
     #     self.set_device_property('shot_time', str(value))
     #     self.shot_time_value = value
 
-    def set_config(self):
+    def set_server_config(self):
         try:
             # log level
             self.logger.setLevel(self.config.get('log_level', logging.DEBUG))
@@ -96,48 +96,71 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
         if param is None:
             param = {}
         # check if attribute exists
-        if name in self.config:
-            return self.config[name]
-        conf = {'ready': False, 'attribute': None, 'local_name': name.replace('/', '_'), 'device_proxy': None}
-        d_n, a_n = TangoAttributeHistoryServer.split_attribute_name(name)
-        conf['device_name'] = d_n
-        conf['attribute_name'] = a_n
-        # create device proxy
-        if d_n in TangoAttributeHistoryServer.tango_devices:
-            d_p = TangoAttributeHistoryServer.tango_devices[d_n]
-        else:
-            d_p = tango.DeviceProxy(d_n)
-            d_p.ready = False
-            TangoAttributeHistoryServer.tango_devices[d_n] = d_p
-        # check if device is on
-        if not d_p.ready:
-            try:
-                d_p.ping()
-                d_p.ready = True
-            except:
-                d_p.ready = False
-        conf['device_proxy'] = d_p
-        if not d_p.ready:
-            self.logger.debug('Device is off for %s', name)
-            return conf
-        # check if remote attribute exists
-
-        # create local attribute
-
-        # check if remote attribute polled
-
+        if name in self.attributes:
+            self.logger.debug('Attribute for %s exists', name)
+            return self.attributes[name]
+        conf = {'ready': False, 'attribute': None, 'local_name': name.replace('/', '_'),
+                'device_proxy': None, 'period': -1}
         try:
-            if not d_p.is_attribute_polled(a_n):
-                period = d_p.get_attribute_poll_period(a_n)
-                d_p.poll_attribute(a_n, period)
-                self.logger.debug('Polling has been restarted for %s', name)
+            d_n, a_n = TangoAttributeHistoryServer.split_attribute_name(name)
+            conf['device_name'] = d_n
+            conf['attribute_name'] = a_n
+            # create device proxy
+            if d_n in TangoAttributeHistoryServer.tango_devices:
+                d_p = TangoAttributeHistoryServer.tango_devices[d_n]
+            else:
+                d_p = tango.DeviceProxy(d_n)
+                d_p.ready = False
+                TangoAttributeHistoryServer.tango_devices[d_n] = d_p
+            # check if device is on
+            if not d_p.ready:
+                try:
+                    d_p.ping()
+                    d_p.ready = True
+                except:
+                    d_p.ready = False
+            conf['device_proxy'] = d_p
+            if not d_p.ready:
+                self.logger.debug('Device is off for %s', name)
+                return conf
+            self.logger.debug('Device for %s is on', name)
+            # check if remote attribute exists
+            try:
+                result = d_p.read_attribute(a_n)
+            except:
+                self.logger.debug('Attribute %s is not readable', name)
+                return conf
+            # check if remote attribute is polled
             period = d_p.get_attribute_poll_period(a_n)
             conf['period'] = period
-            ##depth = d_p.get_attr_poll_ring_depth(a_n)
-            conf['depth'] = depth
+            if not d_p.is_attribute_polled(a_n):
+                d_p.poll_attribute(a_n, period)
+                period = d_p.get_attribute_poll_period(a_n)
+            conf['period'] = period
             if period <= 0:
                 self.logger.warning('Polling can not be enabled for %s', name)
                 return conf
+            self.logger.debug('Polling has been restarted for %s', name)
+            # create local attribute
+            if conf['attribute'] is None:
+                # create local attribute
+                attr = tango.Attr(conf['local_name'], [[float], [float]], tango.AttrWriteType.READ)
+                self.add_attribute(attr, self.read_attribute)
+                conf['attribute'] = attr
+                # set local attr info according to the remote one
+                info = conf['device_proxy'].get_attribute_config_ex(conf['attribute_name'])
+                #             attr_info_ex(AttributeInfoEx) extended attribute information
+                info = AttributeInfoEx()
+                info.data_format = tango.AttrDataFormat.IMAGE
+                info.data_type = tango.AttrDataFormat.IMAGE
+                info.writable = False
+                self.set_attribute_config(info)
+                self.logger.debug('Attribute %s initialized', conf['local_name'])
+
+
+
+            depth = d_p.get_attr_poll_ring_depth(a_n)
+            conf['depth'] = depth
             if 'delta_t' in param:
                 n = int(param['delta_t'] * 1000.0 / period)
             else:
@@ -161,7 +184,7 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
                         attr = tango.Attr(conf['local_name'], [[float], [float]], tango.AttrWriteType.READ)
                         self.add_attribute(attr, self.read_attribute)
                         conf['attribute'] = attr
-                        # set local attr info according to rwmote arrt
+                        # set local attr info according to the remote one
                         info = conf['device_proxy'].get_attribute_config_ex(conf['attribute_name'])
                         #             attr_info_ex(AttributeInfoEx) extended attribute information
                         info = AttributeInfoEx()
@@ -169,8 +192,8 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
                         info.data_type = tango.AttrDataFormat.IMAGE
                         info.writable = False
                         self.set_attribute_config(info)
+                        self.logger.debug('Attribute %s initialized', conf['local_name'])
                     n += 1
-                    self.logger.debug('Attribute %s initialized', name)
             except:
                 self.log_exception('Attribute %s initialization failure' % name)
         if n > 0:
@@ -180,29 +203,32 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
 
     def read_attribute(self, attr: tango.Attribute):
         name = attr.get_name()
-        conf = self.config.attributes[name]
-        if not conf['ready']:
-            # reconnect to attribute
-           self.configure_attribute(name)
-        if not conf['ready']:
-            msg = 'Cannot reconnect %s' % name
-            self.logger.debug(msg)
-            self.debug_stream(msg)
-            attr.set_quality(tango.AttrQuality.ATTR_INVALID)
-            return EMPTY_HISTORY
-        d_p = conf['device_proxy']
-        a_n = conf['attribute_name']
         try:
+            remote_name = name.replace('_', '/')
+            conf = self.attributes[remote_name]
+            if not conf['ready']:
+                # reconnect to attribute
+               conf = self.configure_attribute(remote_name)
+            if not conf['ready']:
+                msg = 'Cannot reconnect %s' % name
+                self.logger.debug(msg)
+                self.debug_stream(msg)
+                attr.set_quality(tango.AttrQuality.ATTR_INVALID)
+                return EMPTY_HISTORY
+            d_p = conf['device_proxy']
+            a_n = conf['attribute_name']
+
+
             if not d_p.is_attribute_polled(a_n):
-                logger.debug('Polling is disabled for %s', name)
-                return history
+                self.ogger.debug('Polling is disabled for %s', name)
+                return EMPTY_HISTORY
             # test if device is alive
             d_p.ping()
             p_s = TangoAttributeHistoryServer.convert_polling_status(d_p.polling_status(), a_n)
             a = 'depth'
             if p_s[a] <= 0:
-                logger.debug('Polling is disabled for %s', name)
-                return history
+                self.logger.debug('Polling is disabled for %s', name)
+                return EMPTY_HISTORY
             if delta_t is not None:
                 n = int(delta_t * 1000.0 / p_s['period'])
             else:
@@ -218,7 +244,11 @@ class TangoAttributeHistoryServer(TangoServerPrototype):
                 history[i, 1] = d.value
                 history[i, 0] = d.time.totime()
             conf['ready'] = True
-
+            attr.set_quality(tango.AttrQuality.ATTR_VALID)
+        except:
+            attr.set_quality(tango.AttrQuality.ATTR_INVALID)
+            self.log_exception('Error reading %s', name)
+            return EMPTY_HISTORY
 
 
 def post_init_callback():
